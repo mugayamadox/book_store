595b284b759fbce0a9806f1935388fb18cded4b8
inverse <mugayamadox@gmail.com>
2024-09-26 13:53:01 +0300

    ADD route for querying books in the database

--- backend/index.js
+++ backend/index.js
@@ -6,10 +6,12 @@ const app = express();
 
 app.use(express.json());
 
+//define default route
 app.get("/", (request, response) => {
   console.log(request);
   return response.status(234).send("welcome to MERN stack");
 });
+
 //create route for adding a book
 app.post( '/book', async (request, response)=>{
     try{
@@ -35,6 +37,23 @@ app.post( '/book', async (request, response)=>{
       response.status(500).send({message: error.message});
     }
 });
+
+//create route for fetching books in the database
+app.get( '/book', async (request, response)=>{
+  try{
+    const books = await Book.find({});
+    return response.status(200).json({
+      count: books.length,
+      data:books});
+
+  }catch(error){
+    console.log(error.message);
+    response.status(500).send({message: error.message})
+  }
+
+});
+
+//connect to database
 mongoose
   .connect(mongoDBURL)
   .then(() => {
