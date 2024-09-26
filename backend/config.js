030e9d40e61d778784b0b4f6c375bcf8e5a53b30
inverse <mugayamadox@gmail.com>
2024-09-26 04:55:05 +0300

    Create Database, Add route for adding books

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,3 @@
 export const PORT = 5555;
 
-const mongoDBURL= 'mongodb+srv://mugayamadox:VYEUaFynmw70Dnnz@book-mern.lvh62.mongodb.net/book-collection?retryWrites=true&w=majority&appName=book-mern'
+export const mongoDBURL= 'mongodb+srv://mugayamadox:VYEUaFynmw70Dnnz@book-mern.lvh62.mongodb.net/book-collection?retryWrites=true&w=majority&appName=book-mern'
